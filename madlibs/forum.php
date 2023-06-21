0cd9d145d530f0651feffcbe336ce5433587d92a
CasEuser1 <113170768+CasEuser1@users.noreply.github.com>
2023-06-21 09:17:56 +0200

    .

---
+++ madlibs/forum.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <label for="kunnen">Wat zou je graag willen kunnen?</label>
+  <input type="text" name="name" required><br>
+
+  <label for="mogen">Met welk persoon kan je goed opschieten?</label>
+  <input type="" name="" required><br>
+  <label for="getal">Wat is je favoriete getal?</label>
+  <input type="" name="" required><br>
+  <label for="eigenschap">Wat is je beste persoonlijke eigenschap?</label>
+  <input type="" name="" required><br>
+  <label for="mogen"> Wat is je slechtse persoonlijke eigenschap?</label>
+  <input type="" name="" required><br>
+  <label for="vakantie">Wat heb je altijd bij als je op vakantie gaat?</label>
+  <input type="" name="" required><br>
+  <label for="ergste">Wat is het ergste wat je kan overkomen?</label>
+  <input type="" name="" required><br>
+
+
+  <input type="submit" value="versturen">
+</form>
